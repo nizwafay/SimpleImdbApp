68a448e4fa2bb2c4d825af081dc6545283e34a95
nizwafay <muhammadnizwa@gmail.com>
2024-02-25 06:14:15 +0700

    init genres screen

---
+++ app/src/main/java/com/example/simpleimdbapp/ui/components/ImdbTopAppBar.kt
@@ -0,0 +1,34 @@
+package com.example.simpleimdbapp.ui.components
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ImdbTopAppBar(title: String) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(
+                title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+    )
+}
+
+@Preview
+@Composable
+fun ImdbTopAppBarPreview() {
+    ImdbTopAppBar(title = "Genres")
+}
